71258dbad427e4a9be2d764a7685dd3b9e90570c
rsjohnson <reagansjohnson4js@gmail.com>
2026-04-13 13:28:43 -0500

    Spots Stage 5: Form Submission

--- scripts/index.js
+++ scripts/index.js
@@ -4,8 +4,22 @@ const editProfileCloseBtn = editProfileModal.querySelector(
   ".modal__close-button",
 );
 
+const profileFormElement = editProfileModal.querySelector(".modal__form");
+const EditProfileNameInput = editProfileModal.querySelector(
+  "#profile-name-input",
+);
+const EditProfileDescriptionInput = editProfileModal.querySelector(
+  "#profile-description-input",
+);
+
+const profileNameEl = document.querySelector(".profile__name");
+const profileDescriptionEl = document.querySelector(".profile__description");
+
 editProfileBtn.addEventListener("click", function () {
   editProfileModal.classList.add("modal_is-opened");
+
+  EditProfileNameInput.value = profileNameEl.textContent;
+  EditProfileDescriptionInput.value = profileDescriptionEl.textContent;
 });
 
 editProfileCloseBtn.addEventListener("click", function () {
@@ -23,3 +37,27 @@ newPostBtn.addEventListener("click", function () {
 newPostCloseBtn.addEventListener("click", function () {
   newPostModal.classList.remove("modal_is-opened");
 });
+function handleProfileFormSubmit(evt) {
+  evt.preventDefault(); //prevents default HTML form behavior
+
+  profileNameEl.textContent = EditProfileNameInput.value;
+  profileDescriptionEl.textContent = EditProfileDescriptionInput.value;
+  editProfileModal.classList.remove("modal_is-opened");
+}
+
+profileFormElement.addEventListener("submit", handleProfileFormSubmit);
+
+const addCardFormElement = newPostModal.querySelector(".modal__form");
+const linkInput = newPostModal.querySelector("#card-image-input");
+const captionInput = newPostModal.querySelector("#card-caption-input");
+
+function handleAddCardSubmit(evt) {
+  evt.preventDefault();
+
+  console.log(linkInput.value);
+  console.log(captionInput.value);
+
+  newPostModal.classList.remove("modal_is-opened");
+}
+
+addCardFormElement.addEventListener("submit", handleAddCardSubmit);
